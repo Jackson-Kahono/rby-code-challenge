bfc63afb43d87592082359e00a57351e9e90720d
Jackson-Kahono <jackson.kahono@student.moringaschool.com>
2022-10-30 22:40:38 +0300

    Completed assignment

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,27 @@
 class Article
 
+  attr_accessor :magazine, :title#print all magazines
 
+  @@all = []
+
+  def initialize(author,magazine,title)
+    @author = author
+    @magazine = magazine
+    @title = title
+    @@all << self
+  end
+
+  def title
+    @title
+  end
+
+  def self.all
+    @@all
+  end
+
+  #print all authors
+  def author
+    @author
+  end
 end
+
--- tools/console.rb
+++ tools/console.rb
@@ -7,13 +7,38 @@ end
 
 ###  WRITE YOUR TEST CODE HERE ###
 
+def main
+  author = Author.new("Stephen King")
+  puts author.name
 
+  magazine = Magazine.new("Time","News")
+  puts magazine.name
 
+  article = Article.new(author, magazine, "The Shining")
+  puts article.title
 
+  Article.all.each {|article| puts article.title}
 
+  Magazine.all.each {|magazine| puts magazine.name}
 
+  puts magazine.contributors
 
+  puts magazine.find_by_name("Time")
 
+  puts magazine.artile_titles
+
+  puts magazine.contributing_authors
+
+  puts author.articles
+
+  puts author.magazines
+
+  author.add_article(magazine, "The Shining")
+
+  puts author.topic_areas
+
+  
+end
 
 ### DO NOT REMOVE THIS
 binding.pry
